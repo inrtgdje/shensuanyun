ecc89f89bb5adafa94186b1fc15e3546e6e934a5
md101 <tts1988@163.com>
2017-04-07 23:28:17 +0800

    Revert "ggg"
    This reverts commit d29f270a6e47f4a49217a4fa4d37fe4b30f9b362.

--- test/test/ViewController.h
+++ test/test/ViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
+@property (nonatomic,strong) NSMutableArray *dataArray;
 
 @property (nonatomic,strong) NSMutableArray *dataArray1;
 @property (nonatomic,strong) NSMutableArray *dataArray2;
