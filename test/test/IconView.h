6dae25bbb86aa32ec2f2287bdad11bc7c221969a
汤天明 <ttm0402@163.com>
2017-04-07 23:25:30 +0800

    Revert "Revert "添加视图""
    This reverts commit a3e4033c04322fddf6c68c509a1a9ad3d5b19413.

---
+++ test/test/IconView.h
@@ -0,0 +1,13 @@
+//
+//  IconView.h
+//  test
+//
+//  Created by 汤天明 on 2017/4/7.
+//  Copyright © 2017年 汤天明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IconView : UIView
+
+@end
