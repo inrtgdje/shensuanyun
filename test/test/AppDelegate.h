9fe51927c2151ccad0ade32e5e0b25c82080a1f4
汤天明 <ttm0402@163.com>
2017-04-07 22:10:13 +0800

    初始化项目

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 汤天明 on 2017/4/7.
+//  Copyright © 2017年 汤天明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
