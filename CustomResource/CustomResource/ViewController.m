2089f9d32c4935f6fbdb66eee6fc65732ba9c505
汤天明 <ttm0402@163.com>
2019-01-08 15:55:48 +0800

    修改

--- CustomResource/CustomResource/ViewController.m
+++ CustomResource/CustomResource/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
-
+#import <JavaScriptCore/JavaScriptCore.h>
+#import "TeachSet.h"
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+@property (nonatomic,strong) JSContext *context;
 
 @end
 
@@ -16,9 +20,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+//    JSContext *context = [_webView valueForKeyPath:@"documentView.webView.mainFrame.JSContext"];
+    self.context = [[JSContext alloc]init];
+//    self.context = context;
+    self.context.name = @"lefex.context";
+    [self left_js];
 }
 
+- (void)add_js{
+    
+    NSString *addjs = @"function add(a,b) { return a + b;}; add(1,3)";
+    [self.context evaluateScript:addjs withSourceURL:[NSURL URLWithString:@"add.js"]];
+    //    NSLog(@"sum: %@",@([sumValue toInt32]));
+    JSValue *resultVaule = [self.context[@"add"] callWithArguments:@[@2,@4]];
+    NSLog(@"result: %@",@([resultVaule toInt32]));
+}
+
+
+- (void)left_js{
+    
+    NSString *js = @"var name = \"Lefe_x\";var log_name = function(aname){var res = 'Hello' + aname; console.log(res);};log_name(name);var age = 24;var sum_age = age + 1;";
+    [self.context evaluateScript:js withSourceURL:[NSURL URLWithString:@"left.js"]];
+    NSLog(@"%@",[self.context[@"name"] toString]);
+    NSLog(@"%@",[self.context[@"sum_age"] toString]);
+    [self.context setObject:@"objectC" forKeyedSubscript:@"ObjectC_add"];
+    [self.context setExceptionHandler:^(JSContext *context, JSValue *exception) {
+        NSLog(@"%@",exception);
+    }];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
---
+++ CustomResource/CustomResource/left.js
@@ -0,0 +1,8 @@
+var name = "Lefe_x";
+var log_name = function(aname) {
+    var res = 'Hello' + aname;
+    console.log(res);
+};
+log_name(name);
+var age = 24;
+var sum_age = age + 1;
